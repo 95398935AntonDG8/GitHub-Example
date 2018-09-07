e52f950d8a914f1d53c957731d7659e3a3f1ed4c
Your Name <you@example.com>
2018-09-07 13:24:02 -0600

    vjhjv

---
+++ GitHub Example/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GitHub Example
+//
+//  Created by Anton Miguel de Guia on 2018-09-07.
+//  Copyright © 2018 Anton Miguel de Guia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
